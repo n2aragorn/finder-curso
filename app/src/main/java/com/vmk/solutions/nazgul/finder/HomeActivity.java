18c35e0b7b62d05c04afd4c819ac2ad6befee184
Nazgul <n2aragorn@gmail.com>
2019-09-10 20:39:13 -0300

    Inicializacion del Proyecto

---
+++ app/src/main/java/com/vmk/solutions/nazgul/finder/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.vmk.solutions.nazgul.finder;
+
+import android.os.Bundle;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
