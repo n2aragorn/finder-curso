33c222208dd7b336469b9adf3ebd7bcf451affad
Nazgul <n2aragorn@gmail.com>
2019-09-10 21:41:24 -0300

    Se Agrego Primer Mapa

--- app/src/main/java/com/vmk/solutions/nazgul/finder/HomeActivity.java
+++ app/src/main/java/com/vmk/solutions/nazgul/finder/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.vmk.solutions.nazgul.finder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends BaseActivity {
 
@@ -8,5 +11,15 @@ public class HomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btMap = findViewById(R.id.btMap);
+
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
